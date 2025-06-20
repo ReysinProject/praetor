e9a5b7ac6971b25261b1775b007110b943e08a8a
l.guerra-rago <l.guerra-rago@reysin.fr>
2025-06-20 11:56:37 +0200

    wip

---
+++ src/validation/fields/email_field.cr
@@ -0,0 +1,21 @@
+require "./string_field"
+
+module Validation::Fields
+  # Specialized field for email strings
+  class EmailField < StringField
+    def initialize(
+      default : String? = nil,
+      required : Bool = true,
+      strict : Bool = false,
+      description : String? = nil
+    )
+      super(
+        default: default,
+        required: required,
+        description: description,
+        strict_email: strict,
+        email: !strict # fallback to basic if strict is false
+      )
+    end
+  end
+end
